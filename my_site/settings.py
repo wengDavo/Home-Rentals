692831f88600915a21d63242a7d4bbfc07b28f5d
weng davo <immanueldavo@example.com>
2023-04-12 12:46:03 +0100

    changed debug to false

--- my_site/settings.py
+++ my_site/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 
