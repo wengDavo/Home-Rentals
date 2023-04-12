c012f3c06d5891686f1a5e5435f2d2f6bb29b05f
weng davo <immanueldavo@example.com>
2023-04-12 20:35:25 +0100

    modified media files

--- my_site/settings.py
+++ my_site/settings.py
@@ -116,13 +116,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Base URL to serve media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / '/media/'
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
--- my_site/urls.py
+++ my_site/urls.py
@@ -18,14 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('rentals.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
