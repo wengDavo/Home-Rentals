c819594e100af180948fcee5324c1d625adaf101
weng davo <immanueldavo@example.com>
2023-04-12 12:51:42 +0100

    changed path to media

--- my_site/urls.py
+++ my_site/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
 
     path('', include('rentals.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
+
+# urlpatterns += staticfiles_urlpatterns()
 
